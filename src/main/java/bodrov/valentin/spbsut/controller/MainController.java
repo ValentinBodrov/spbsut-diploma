d13c8f9920b4af0949d06c18f02c24edc4a6f3af
Valentine <befearless4428876@gmail.com>
2020-01-30 18:16:07 +0300

    Added a filechooser for getting filepath. Also code cleanup

--- src/main/java/bodrov/valentin/spbsut/controller/MainController.java
+++ src/main/java/bodrov/valentin/spbsut/controller/MainController.java
@@ -3,9 +3,9 @@ package bodrov.valentin.spbsut.controller;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -14,30 +14,20 @@ import java.io.IOException;
 
 public class MainController {
 
-    public Label helloWorld;
     public Button helloWorldSay;
     public ImageView sampleImage;
 
-
     public void sayHelloWorld(ActionEvent actionEvent) {
-        helloWorld.setText("Hello world!");
-
-        int width = 960;    //width of the image
-        int height = 760;   //height of the image
-
-        // For storing image in RAM
+        int width = 960;
+        int height = 760;
         BufferedImage image = null;
 
-        // READ IMAGE
         try {
-            File input_file = new File("src/main/java/bodrov/valentin/spbsut/sample.jpg"); //image file path
-
-            image = new BufferedImage(width, height,
-                    BufferedImage.TYPE_INT_ARGB);
-
-            image = ImageIO.read(input_file);
-
-            System.out.println("Reading complete.");
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setInitialDirectory(new File("src/main/resources/images/"));
+            File inputFile = fileChooser.showOpenDialog(null);
+            image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            image = ImageIO.read(inputFile);
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
@@ -45,9 +35,6 @@ public class MainController {
         Image imageToImport = SwingFXUtils.toFXImage(image, null);
         sampleImage.setImage(imageToImport);
         sampleImage.setPreserveRatio(false);
-        double fitHeight = sampleImage.getFitHeight();
-        double fitWidth = sampleImage.getFitWidth();
-        System.out.println(fitHeight + " : " + fitWidth);
-        System.out.println("Writing complete.");
     }
+
 }
